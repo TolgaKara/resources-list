c8082f7448c58713f0273a487cb54843802a1282
TolgaKara <tolgakara2.0@gmail.com>
2022-04-12 05:51:11 +0200

    Interim Changes Added

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,22 @@ import { Icons } from '../src/components/Icons';
 import { businessDropdownItems } from '../src/data/business';
 import { webDropdownItems } from '../src/data/web-development';
 import styles from '../styles/Home.module.css';
-import { loadDB } from '../src/lib/firebaseConfig';
-
-import { app, db } from '../src/lib/firebaseConfig';
+import { loadDB, app, db } from '../src/lib/firebaseConfig';
 import { collection, addDoc, getDoc } from 'firebase/firestore';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-export default function Home({ resources }) {
+export default function Home() {
+	const [resources, setResources] = useState(null);
 	console.log(resources);
+	// On Mount call the api resources and set the state
+	useEffect(() => {
+		axios.get('/api/resources').then((res) => {
+			console.log(res.data);
+			setResources(res.data);
+		});
+	}, []);
+
 	return (
 		<Container>
 			<div className='pb-2 border-b-4 border-green-300 w-fit flex gap-4 flex-col items-center justify-center mx-auto md:flex-row'>
@@ -27,16 +36,30 @@ export default function Home({ resources }) {
 				<FilterButton>🎨 UX & UI</FilterButton>
 				<FilterButton dropdownItems={businessDropdownItems}>💼 Business</FilterButton>
 			</div>
+			<div className='mt-8 flex flex-col gap-4 items-center justify-center'>
+				{resources &&
+					resources.map((resource) => {
+						return (
+							<div key={resource.id} className='flex flex-col gap-4 items-center justify-center'>
+								<div className='flex flex-col gap-4 items-center justify-center'>
+									<h1 className='text-3xl font-bold text-center'>{resource.name}</h1>
+								</div>
+								<div className='flex flex-col gap-4 items-center justify-center'>
+									<h1 className='text-3xl font-bold text-center'>{resource.description}</h1>
+								</div>
+								<div>
+									{resource.tags.map((tag) => {
+										return (
+											<div key={tag} className='flex flex-col gap-4 items-center justify-center'>
+												<h1 className='text-3xl font-bold text-center'>{tag}</h1>
+											</div>
+										);
+									})}
+								</div>
+							</div>
+						);
+					})}
+			</div>
 		</Container>
 	);
-}
-
-export async function getStaticProps() {
-	const dbInstance = collection(db, 'resources');
-	console.log(dbInstance);
-	return {
-		props: {
-			resources: await JSON.stringify(dbInstance),
-		},
-	};
 }
